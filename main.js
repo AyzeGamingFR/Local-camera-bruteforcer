e75ecc702738df1735032f8a06e1cbfd928c25b0
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-12-27 09:52:09 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const net = require("net");
 
 // server and client vars
 
-const local_web_server = new net.Server();
 const local_camera_client = new net.socket();
+const local_web_server = new net.Server();
 
-var CAMERA_IP = "127.0.0.x";
+var CAMERA_IP = "192.168.1.PASTE_END_IP_CAMERA_IP_HERE";
 var CAMERA_PORT = 0;
+var WEB_SERVER_PORT = 80;
 
 
 // server and client code
@@ -17,5 +18,11 @@ var CAMERA_PORT = 0;
 local_camera_client.on({
     
     local_camera_client.connect(CAMERA_IP, CAMERA_PORT);
+    
+});
+
+local_web_server.on({
+    
+    local_web_server.listen(WEB_SERVER_PORT);
     
 });

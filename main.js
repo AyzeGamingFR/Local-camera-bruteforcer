1d69f89c4869fe610370b31bdf48c615b528a898
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-12-27 10:34:38 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // requirements
 
-const net = require("net");
+const net = require("net"),
+      http = require("http");
 
 
 // server and client vars
 
-const local_camera_client = new net.socket();
-const local_web_server = new net.Server();
+const local_camera_client = new net.socket();,
+      local_web_server = new http.Server();
 
-var CAMERA_IP = "192.168.1.PASTE_END_IP_CAMERA_IP_HERE";
-var CAMERA_PORT = 0;
-var WEB_SERVER_PORT = 80;
+var CAMERA_IP = "192.168.1.PASTE_END_IP_CAMERA_IP_HERE",
+    CAMERA_PORT = 0,
+    WEB_SERVER_PORT = 80;
 
 
 // server and client code
@@ -21,7 +22,7 @@ local_camera_client.on({
     console.log(`The camera client has been started and connected himself to the camera using the ip ${CAMERA_IP} on the port ${CAMERA_PORT}`);
     for (camera_datas.received != ``) {
         
-        local_camera_client.send();
+        local_camera_client.send(``);
         
     };
     
@@ -30,6 +31,11 @@ local_camera_client.on({
 local_web_server.on({
     
     local_web_server.listen(WEB_SERVER_PORT);
-    console.log(`The camera bruteforcer web server is running on the port ${WEB_SERVER_PORT} and is ready to receive a client !`);
+    console.log(`The bruteforcer local web server is running on the port ${WEB_SERVER_PORT} and is ready to receive a client !`);
+    when () {
+        
+        
+        
+    };
     
 });
